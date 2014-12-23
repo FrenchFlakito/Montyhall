d7271d23390d1752b42b22444450c2a35b36f7da
FrenchFlakito <guillaume01.simplon@gmail.com>
2014-12-24 00:10:30 +0100

    complétion de l'expérience, apparemment NOT ok so far

--- montyhall.rb
+++ montyhall.rb
@@ -5,17 +5,24 @@ def experience
 	truth = rand(3)
 	guess = rand(3)
 
-	puts("Tu as choisi l\'option #{guess + 1}")
+#	puts("Tu as choisi l\'option #{guess + 1}")
 	revealed_box = reveal(truth, guess) + 1
-	puts("Maintenant, je te dis qu\'il n\'y avait rien dans la boîte #{revealed_box}")
+#	puts("Maintenant, je te dis qu\'il n\'y avait rien dans la boîte #{revealed_box}")
 
 	# stratégie "je change"
-	puts("Tu changes de boîte ? Très bien, tu choisis donc désormais la boîte #{ ( box_numbers - [guess] - [revealed_box - 1])[0] + 1}.")
+	new_answer = (box_numbers - [guess] - [revealed_box - 1])[0] + 1
+#	puts("Tu changes de boîte ? Très bien, tu choisis donc désormais la boîte #{new_answer}.")
+
+	if new_answer == truth
+		puts "you win"
+	else
+		puts "Raté !"
+	end
 
 end
 
 def reveal(truth, guess)   # à optimiser !!	
-	revealed = rand(3)	
+	revealed = rand(3)
 	if truth == guess
 		revealed = rand(3) while revealed == truth
 	else
@@ -24,4 +31,4 @@ def reveal(truth, guess)   # à optimiser !!
 	return revealed
 end
 
-experience
+20.times do experience end
